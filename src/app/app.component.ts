347f80a43f17aeda54d88495550748a49ec68ebd
goldshoot0720 <goldshoot0720@gmail.com>
2025-04-20 02:02:14 +0800

    歡迎來到訂閱資訊管理

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {MainComponent} from './main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule,MainComponent],  // 確保 My0000Component 正確匯入
+  imports: [RouterModule],  // 確保 My0000Component 正確匯入
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],  // 修正 styleUrls
   standalone: true,
