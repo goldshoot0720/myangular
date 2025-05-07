af34c3d29c353e40002eaccedca1dbebf68a2c1d
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-07 17:10:31 +0800

    myangular new init 03

--- myangular git.bat
+++ myangular git.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\myangular"
 git init
 git add .
-git commit -m "myangular new init 01"
+git commit -m "myangular new init 03"
 git remote add origin https://github.com/goldshoot0720/myangular.git
 git checkout -b master
 git push -u origin master --force
